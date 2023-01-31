08e0c7cc77426cef127ce9172255eb0d0d5b6658
Trickkyyy <vandark5@gmail.com>
2023-01-31 23:48:31 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './card.js';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots }  from "./robots";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
+    
+    <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email} />
+    
+    <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email} />
+    
+    <Card id={robots[3].id} name={robots[3].name} username={robots[3].username} email={robots[3].email} />
   </React.StrictMode>
 );
 
